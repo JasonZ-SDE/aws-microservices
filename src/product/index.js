5c552fc88f763220ea014c78614494d0c94a3e96
Jason Zheng <hanguang.sde@gmail.com>
2022-08-01 23:15:29 -0400

    Develop product lambda function with SDK - part 1

--- src/product/index.js
+++ src/product/index.js
@@ -1,5 +1,8 @@
 exports.handler = async function (event) {
   console.log("request:", JSON.stringify(event, undefined, 2));
+
+  // TODO: Switch cases event.httpmethod to perform CRUD oprations using DDB obj.
+
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/plain" },
