d687f81d563779b94b646bd4cd17b501b5a4ffe9
Hanguang Zheng <hgzheng@seas.upenn.edu>
2022-08-06 15:39:24 -0400

    Refectored code into resource based gruups

---
+++ lib/database.ts
@@ -0,0 +1,27 @@
+import { RemovalPolicy } from "aws-cdk-lib";
+import {
+  AttributeType,
+  BillingMode,
+  ITable,
+  Table,
+} from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
+
+export class SwnDatabase extends Construct {
+  public readonly productTable: ITable;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    // Product DynamoDB table creation
+    this.productTable = new Table(this, "product", {
+      partitionKey: {
+        name: "id",
+        type: AttributeType.STRING,
+      },
+      tableName: "product",
+      removalPolicy: RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
+  }
+}
